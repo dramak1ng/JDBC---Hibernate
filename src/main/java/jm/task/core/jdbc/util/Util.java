63684ac32f6eb55c99ce0402e0f90a37e90394cd
dramak1ng <kensi11qw@gmail.com>
2025-02-07 19:31:57 +0300

    complete task jdbc 1.1.3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,46 @@
 package jm.task.core.jdbc.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final Logger log = Logger.getLogger(Util.class.getName());
+
+    private static String url;
+    private static String username;
+    private static String password;
+
+    public static Connection getConnection()  {
+
+        try (FileInputStream fis = new FileInputStream("src\\main\\resources\\db.properties")) {
+            Properties properties = new Properties();
+            properties.load(fis);
+            url = properties.getProperty("db.url");
+            username = properties.getProperty("db.username");
+            password = properties.getProperty("db.password");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+
+            log.info("установлено.");
+        } catch (SQLException e) {
+            log.info("oшибка при подключении к базе данных: ");
+            e.printStackTrace();
+
+        }
+        return connection;
+    }
 }
